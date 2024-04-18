0f126b3e36556ee52ebf211d216271bc3c3fdb6f
Amos .O. Odundo <114424543+Amos202-create@users.noreply.github.com>
2024-04-18 17:59:25 +0300

    Add files via upload

---
+++ port/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+    // Here you can add code to handle form submission, like sending an email or storing data in a database
+    alert('Thank you for contacting Us!');
+});
